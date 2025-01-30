875f9cd353f608c243598289762281138309948d
perryboyce <p.boyce1212@gmail.com>
2025-01-30 00:02:37 -0500

    basic movement added

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    main_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +18,8 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
-
+        main_clock.tick(60)
+        dt = main_clock.tick() / 1000
+        
 if __name__ == "__main__":
     main()
